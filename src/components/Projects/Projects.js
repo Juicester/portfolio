cd1c25fd62be74868cd570731bde1485c46193b8
justinpedowitz <justin@pedowitz.net>
2020-11-12 14:14:11 -0800

    worked out links for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import './Projects.css';
+import Maze from '../../images/Maze.png'
+import AdelphiMap from '../../images/startscreen-adelphi.png'
+import Knockdown from '../../images/Knockdown.png'
+import Terrain from '../../images/Terrain.png'
+import Galaxor from '../../images/Galaxor.png'
+import SpiritBomb from '../../images/spiritbomb.png'
+import Rubiks from '../../images/rubicscube2.png'
 
 function Projects() {
 	return (
@@ -14,17 +22,22 @@ function Projects() {
 				<Row className='row1'>
 					<Col className='proj1'>
 						<Card>
-							<Card.Title>Project 1</Card.Title>
+							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/assets/maze_html/Maze.html'}} target='_blank'>
+								<Card.Title>Project 1</Card.Title>
+								<Card.Img src={Maze}/>
+							</Link>
 						</Card>
 					</Col>
 					<Col className='proj2'>
 						<Card>
 							<Card.Title>Project 2</Card.Title>
+							<Card.Img src={AdelphiMap}/>
 						</Card>
 					</Col>
 					<Col className='proj3'>
 						<Card>
 							<Card.Title>Project 3</Card.Title>
+							<Card.Img src={Knockdown}/>
 						</Card>
 					</Col>
 				</Row>
@@ -32,18 +45,30 @@ function Projects() {
 					<Col className='proj4'>
 						<Card>
 							<Card.Title>Project 4</Card.Title>
+							<Card.Img src={Terrain}/>
 						</Card>
 					</Col>
 					<Col className='proj5'>
 						<Card>
 							<Card.Title>Project 5</Card.Title>
+							<Card.Img src={Galaxor}/>
 						</Card>
 					</Col>
 					<Col className='proj6'>
 						<Card>
 							<Card.Title>Project 6</Card.Title>
+							<Card.Img src={SpiritBomb}/>
 						</Card>
 					</Col>
+				</Row>
+				<Row className='row3'>
+					<Col className='proj7'>
+						<Card>
+							<Card.Title>Project 7</Card.Title>
+							<Card.Img src={Rubiks}/>
+						</Card>
+					</Col>
+					
 				</Row>
 			</Container>
 		</div>
