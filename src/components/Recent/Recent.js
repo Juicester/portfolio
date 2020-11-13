a3a4e0c3c6ebe64986fefd5672dbfbf0f1346f01
justinpedowitz <justin@pedowitz.net>
2020-11-13 15:35:35 -0800

    added more to recent works

--- src/components/Recent/Recent.js
+++ src/components/Recent/Recent.js
@@ -5,11 +5,10 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import './Recent.css';
-import Maze from '../../images/Maze.png';
-import AdelphiMap from '../../images/startscreen-adelphi.png';
-import Knockdown from '../../images/Knockdown.png';
-import Terrain from '../../images/Terrain.png';
-import Galaxor from '../../images/Galaxor.png';
+import NFLAnalysis from '../../images/NFLAnalysisApp.png';
+import PathfinderStore from '../../images/pathfinderhomepage.png';
+import HallofGames from '../../images/HallOfGames.png';
+import Hangman from '../../images/hangman.jpeg';
 
 function Recent() {
 	return (
@@ -21,8 +20,8 @@ function Recent() {
 					<Col className='proj1'>
 						<Card>
 							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/assets/maze_html/Maze.html'}} target='_blank'>
-								<Card.Title>Maze</Card.Title>
-								<Card.Img src={Maze}/>
+								<Card.Title>NFL Analysis App</Card.Title>
+								<Card.Img src={NFLAnalysis}/>
 							</Link>
 							<Card.Text>Maze is a Unity game where you need to find your way through a maze to the exit! To play, use the mouse to look around and 'W', 'A', 'S', and 'D' to move.</Card.Text>
 						</Card>
@@ -30,8 +29,8 @@ function Recent() {
 					<Col className='proj2'>
 						<Card>
 							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/AU3DMap.html'}} target='_blank'>
-							<Card.Title>AU 3D Map</Card.Title>
-							<Card.Img src={AdelphiMap}/>
+							<Card.Title>Pathfinder Store</Card.Title>
+							<Card.Img src={PathfinderStore}/>
 							</Link>
 							<Card.Text>My capstone project, a 3D Map of my university. All aspects of the project were made in Unity, SketchUp and Blender.</Card.Text>
 						</Card>
@@ -39,8 +38,8 @@ function Recent() {
 					<Col className='proj3'>
 						<Card>
 							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/assets/knockdown_html/knockdown.html'}} target='_blank'>
-							<Card.Title>Knockdown</Card.Title>
-							<Card.Img src={Knockdown}/>
+							<Card.Title>Hall of Games</Card.Title>
+							<Card.Img src={HallofGames}/>
 							</Link>
 							<Card.Text>Knockdown is a Unity game where you have to launch the ball from the slingshot into the star. To play, mouse over the slingshot, click and drag to move the ball and release to send it flying. Knockdown is based on Angry Birds.</Card.Text>
 						</Card>
@@ -50,19 +49,10 @@ function Recent() {
 					<Col className='proj4'>
 						<Card>
 							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/assets/terrain_html/Terrain.html'}} target='_blank'>
-							<Card.Title>Terrain</Card.Title>
-							<Card.Img src={Terrain}/>
+							<Card.Title>Hangman</Card.Title>
+							<Card.Img src={Hangman}/>
 							</Link>
 							<Card.Text>Terrain is a Unity game used to show off our terrain building skills. To play, use the mouse to look around and 'W', 'A', 'S', and 'D' to move.</Card.Text>
-						</Card>
-					</Col>
-					<Col className='proj5'>
-						<Card>
-							<Link to={{ pathname: 'https://home.adelphi.edu/~ju21229/assets/galaxor_html/Galaxor.html'}} target='_blank'>
-							<Card.Title>Galaxor</Card.Title>
-							<Card.Img src={Galaxor}/>
-							</Link>
-							<Card.Text>Galxor is a Unity game based on Galaga, where you have to shoot enemies while dodging their bullets. To play, use 'W', 'A', 'S' and 'D' to move and 'Spacebar' to shoot.</Card.Text>
 						</Card>
 					</Col>
 				</Row>
