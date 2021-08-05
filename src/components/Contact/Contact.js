981f18559da8024514e8919d9223bf89fc50251b
justinpedowitz <justin@pedowitz.net>
2021-08-05 16:18:25 -0700

    more contact stuff

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,28 +9,28 @@ import Button from 'react-bootstrap/Button'
 function Contact() {
   return (
 		<Container>
-			<h1>Hello From Contact</h1>
-
+			<h1>Thank you for taking time to reach out to me.  Please leave your message below and I will respond ASAP</h1>
 			<Form>
 				<Row>
 					<Col>
-						<Form.Group>
+						<Form.Group className='form-label'>
 							<Form.Label>Name</Form.Label>
 							<Form.Control type='text' placeholder='Full Name' />
 						</Form.Group>
 					</Col>
 					<Col>
-						<Form.Group>
+						<Form.Group className='form-label'>
 							<Form.Label>Email address</Form.Label>
 							<Form.Control type='email' placeholder='email@example.com' />
 						</Form.Group>
 					</Col>
 				</Row>
-				<Form.Group>
+				<br />
+				<Form.Group className='form-label'>
 					<Form.Label>Message</Form.Label>
-					<Form.Control as='textarea' rows={3} />
+					<Form.Control as='textarea' rows={5} />
 				</Form.Group>
-				<Button variant='primary' type='submit'>
+				<Button className='modal-button' variant='primary' type='submit'>
 					Submit
 				</Button>
 			</Form>
